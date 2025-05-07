36ab11d61835c7eeb80976da881778ba82efc5fe
kim youngkwon <hackk7111@gmail.com>
2025-05-07 09:37:25 +0900

    Add IndexController

---
+++ src/test/java/com/example/teamproject/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.teamproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+public class IndexController {
+    @GetMapping("/")
+    public @ResponseBody String index() {
+        return "Hello, World!!";
+    }
+}
